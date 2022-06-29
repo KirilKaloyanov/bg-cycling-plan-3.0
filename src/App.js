bb4b668f2602dd169f88286aee6e0e7133f15c4d
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2022-06-29 09:24:08 +0300

    Added app.js and index.css files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
-import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/common/navbar";
+import Hero from "./components/common/hero";
+import Page from "./components/page";
+import NavBack from "./components/common/navback";
+import Map from "./components/map";
+import MapRaw from "./components/mapraw";
+import Partners from "./components/partners";
+import Conference from "./components/conference";
+import Ambassadors from "./components/ambassadors";
+import Library from "./components/library";
+import dtpLogo from "./components/images/logoDTP.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isMenuVisible: false,
+    offsetY: 0,
+  };
+
+  setMenuVisibility = () => {
+    let result = { ...this.state };
+    result.isMenuVisible = !this.state.isMenuVisible;
+    this.setState(result);
+  };
+
+  handleScroll = () => {
+    let result = { ...this.state };
+    result.offsetY = window.pageYOffset;
+    this.setState(result);
+  };
+
+  resetScroll = () => {
+    window.scrollY(0);
+    let result = { ...this.state };
+    result.isMenuVisible = false;
+    this.setState(result);
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  render() {
+    const { isMenuVisible: toggle, offsetY } = this.state;
+    return (
+      <>
+        <NavBar
+          toggle={toggle}
+          offsetY={offsetY}
+          onToggle={this.setMenuVisibility}
+          onReset={this.resetScroll}
+        />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route
+            path="page"
+            element={
+              <>
+                <NavBack toggle={toggle} />
+                <Page />
+              </>
+            }
+          />
+          <Route
+            path="map"
+            element={
+              <>
+                <NavBack toggle={toggle} />
+                <Map />
+              </>
+            }
+          />
+          <Route
+            path="94afG4qf3Rcpasdf"
+            element={
+              <>
+                <NavBack toggle={toggle} />
+                <MapRaw />
+              </>
+            }
+          />
+          <Route
+            path="partners"
+            element={
+              <>
+                <NavBack toggle={toggle} />
+                <Partners />
+              </>
+            }
+          />
+          <Route
+            path="conference"
+            element={
+              <>
+                <NavBack toggle={toggle} />
+                <Conference />
+              </>
+            }
+          />
+          <Route
+            path="ambassadors"
+            element={
+              <>
+                <NavBack toggle={toggle} />
+                <Ambassadors />
+              </>
+            }
+          />
+          <Route
+            path="library"
+            element={
+              <>
+                <NavBack toggle={toggle} />
+                <Library />
+              </>
+            }
+          />
+        </Routes>
+        <div className="footer">
+          <img
+            className="dtpLogo"
+            src={dtpLogo}
+            alt="Danube Transnational Programme logo"
+          />
+          <div className="funding">
+            Project is co-funded by the European Union funds (ERDF,IPA)
+          </div>
+        </div>
+      </>
+    );
+  }
 }
-
 export default App;
